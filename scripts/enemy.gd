0f150008bdd0bd201a6b30d6ad5cfcd5a588d997
needJobCoder <97464694+needJobCoder@users.noreply.github.com>
2023-08-06 21:44:04 +0530

    navigation testing with enemy

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,6 +1,17 @@
 extends CharacterBody2D
 
+@onready var navAgent : NavigationAgent2D = $NavigationAgent2D
 
+var SPEED = 30
 
-var SPEED = 40
-
+func _physics_process(delta):
+	navigate()
+	
+func navigate():
+	navAgent.target_position = get_global_mouse_position()
+	var direction : Vector2 = navAgent.get_next_path_position() - self.global_position
+	
+	velocity = direction.normalized() * SPEED
+	print(velocity)
+	move_and_slide()
+	
